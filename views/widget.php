611acfd74168bd2aaf3a5c6229c2391fd04fa2d1
jappi00 <jappi2000@ewetel.net>
2016-07-06 23:20:19 +0200

    Update widget.php

--- views/widget.php
+++ views/widget.php
@@ -1,9 +1,7 @@
 <?php
 use Pagekit\Application as App;
 $view->script('theme', 'twitter:js/twitterapi.js');
-var_dump(App::module('twitter')->config('transparent'));
 $settings = App::module('twitter')->config();
-var_dump($settings)
 ?>
 <a class="twitter-timeline" href="https://twitter.com/<?=$settings['name']?>" data-tweet-limit="<?= $settings['number'] ?>" data-chrome="<?php if ($settings['transparent']) {echo 'transparent';}; if ($settings['noborders']) {echo 'noborders';}; if ($settings['noscrollbar']) {echo 'noscrollbar';};?>">
 </a>
