a881cee208ebbc9d04f04ce5966b752d0d98de8f
Jan-Phillip Oesterling <Jan-Phillip Oesterling>
2016-07-07 01:24:42 +0200

    fixes

--- views/widget.php
+++ views/widget.php
@@ -3,5 +3,5 @@ use Pagekit\Application as App;
 $view->script('twitterapi', 'twitter:js/twitterapi.js');
 $settings = App::module('twitter')->config();
 ?>
-<a class="twitter-timeline"<?php if (isset($settings['id'])) {echo 'data-widget-id="'.$settings['id'] . '"';}?>" href="https://twitter.com/<?=$settings['name']?>" data-tweet-limit="<?= $settings['number'] ?>" data-chrome="<?php if ($settings['transparent']) {echo 'transparent';}; if ($settings['noborders']) {echo 'noborders';}; if ($settings['noscrollbar']) {echo 'noscrollbar';};?>">
+<a class="twitter-timeline"<?php if (!empty($settings['id'])) {echo 'data-widget-id="'.$settings['id'] . '"';}?>" href="https://twitter.com/<?=$settings['name']?>" data-tweet-limit="<?= $settings['number'] ?>" data-chrome="<?php if ($settings['transparent']) {echo 'transparent';};?> <?php if ($settings['noborders']) {echo 'noborders';};?>  <?php if ($settings['noscrollbar']) {echo 'noscrollbar';};?>">
 </a>
